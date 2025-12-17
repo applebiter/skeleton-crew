c70d7cf53063692dc3a8c202d20b62a13efc66bf
System Administrator <webmaster@applebiter.com>
2025-12-16 19:18:17 -0500

    Implement node canvas v3 with strict Model-View separation

--- src/skeleton_app/gui/widgets/__init__.py
+++ src/skeleton_app/gui/widgets/__init__.py
@@ -7,5 +7,5 @@ __all__ = ["TransportPanel", "ClusterPanel", "PatchbayWidget", "NodeCanvasWidget
 from skeleton_app.gui.widgets.transport_panel import TransportPanel
 from skeleton_app.gui.widgets.cluster_panel import ClusterPanel
 from skeleton_app.gui.widgets.patchbay_widget import PatchbayWidget
-from skeleton_app.gui.widgets.node_canvas_v2 import NodeCanvasWidget
+from skeleton_app.gui.widgets.node_canvas_v3 import NodeCanvasWidget
 from skeleton_app.gui.widgets.video_panel import VideoPanel
